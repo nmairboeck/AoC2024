bba8666534582dbf1ec40813272ecd21d9869ca9
n.mairboeck@countit.at <n.mairboeck@gmail.com>
2024-12-01 20:48:14 +0100

    Day 1

--- AoC2024.1/Program.cs
+++ AoC2024.1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("AoC 2024 Day 1 Part 1");
+
+var lines = await System.IO.File.ReadAllLinesAsync("Input.txt");
+
+var numbers = lines.Select(l => l.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+
+var left = numbers.Select(n => n[0]).ToList().Order();
+var right = numbers.Select(n => n[1]).ToList().Order();
+
+var distances = left.Zip(right, (l, r) => Math.Abs(r - l));
+
+var distance = distances.Sum();
+
+Console.WriteLine($"The total distance is {distance}");
+
+var occurences = right.GroupBy(d => d).ToDictionary(g => g.Key, g => g.Count());
+
+var similarities = left.Sum(l => (occurences.TryGetValue(l, out var count) ? count : 0) * l);
+
+Console.WriteLine($"The similarity is {similarities}");
